a0c52eb833aa8751898ea78b6749d949db377234
Duy Dang <dangphuocduy@gmail.com>
2023-12-31 12:07:22 +0700

    Example comment

---
+++ Comment.js
@@ -0,0 +1,40 @@
+// Comment (Chú thích)
+/**
+ * Comment một dòng
+ * Comment nhiều dòng
+ * Sử dụng phím tắt
+ * Ctr + /
+ * Khối comment
+ * /**
+ */
+
+// Các bạn xem ví dụ tại đây:
+
+// 1. Dưới đây là một lời gọi hàm alert() bị comments,
+// vì vậy, không có hộp thoại nào xuất hiện.
+// 👉 Bạn có thể uncomment để thấy hộp thoại được bật lên.
+
+// alert('Xin chào anh em nhé!');
+
+
+/* Đây là
+cú pháp
+comment trên nhiều dòng */
+
+/**
+ * Cách trình bày đẹp cho cú pháp comments
+ * trên nhiều dòng là như thế này
+ */
+
+/**
+ * Đây là cách trình bày cú pháp comments nhiều 
+ * Đẹp
+ * Dễ nhìn
+ */
+
+// var fullName = 'Duy Đặng';
+// var address = 'Đà Nẵng'
+
+/*
+Đây là comment nhiều dòng
+ */
