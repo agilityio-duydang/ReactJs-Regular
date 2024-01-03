8112c7ed039405d52df41a9e08602f013997eabe
Duy Dang <dangphuocduy@gmail.com>
2024-01-03 17:43:16 +0700

    Example Json object

---
+++ Json.js
@@ -0,0 +1,13 @@
+/**
+ *  1. Event listeners
+ *  2. JSON
+ *  3. Promise
+ *  4. Fetch
+ *  5. DOM location
+ *  6. Local storage
+ *  7. Session storage
+ *  8. Code convention
+ *  9. Best Practices
+ *  10. Misstakes
+ *  11. Performance
+ */
